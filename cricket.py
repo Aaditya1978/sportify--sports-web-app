16452838a9d14a89d25efcf8ba93d4d0ad149f37
Aaditya1978 <aadityasinghal1978@gmail.com>
2021-02-28 21:47:07 +0530

    Added function to get previous matches

--- cricket.py
+++ cricket.py
@@ -32,29 +32,53 @@ def live_matches():
 
         datas = {}
 
-        if i["coverage"] == "Y":
+        if (i["coverage"] == "Y" or i["coverage"] == "U") and  i["state"] == "LIVE":
             datas['matchId'] = i['objectId']
             datas['seriesId'] = i['series']['objectId']
             datas['match_name'] = i['slug']
             datas['status'] = i['statusText']
-            datas['toss_winner_id'] = i['tossWinnerTeamId']
-            datas['toss_choice'] = i['tossWinnerChoice']
             datas['series_name'] = i['series']['longName']
             datas['stadium'] = i['ground']['longName']
             datas['format'] = i['format']
-            datas['team1'] = {}
-            datas['team2'] = {}
-            datas['team1']['name'] = i['teams'][0]['team']['longName']
-            datas['team1']['id'] = i['teams'][0]['team']['id']
-            datas['team1']['object_id'] = i['teams'][0]['team']['objectId']
-            datas['team1']['score'] = i['teams'][0]['score']
-            datas['team1']['score_info'] = i['teams'][0]['scoreInfo']
-            datas['team2']['name'] = i['teams'][1]['team']['longName']
-            datas['team2']['id'] = i['teams'][1]['team']['id']
-            datas['team2']['object_id'] = i['teams'][1]['team']['objectId']
-            datas['team2']['score'] = i['teams'][1]['score']
-            datas['team2']['score_info'] = i['teams'][1]['scoreInfo']
+            matches.append(datas)
 
+    return matches
+
+
+
+def previous_matches():
+
+    """Return list of the previous matches
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    list
+        a list of previous matches
+    """
+
+    url = os.getenv("CRICKET_PREVIOUS_URL")
+    r = requests.get(url)
+    previous = r.json()
+
+    matches = []
+
+    for i in previous["content"]["matches"]:
+
+        datas = {}
+
+        if (i["coverage"] == "Y" or i["coverage"] == "U"):
+            datas['matchId'] = i['objectId']
+            datas['seriesId'] = i['series']['objectId']
+            datas['match_name'] = i['slug']
+            datas['status'] = i['statusText']
+            datas['series_name'] = i['series']['longName']
+            datas['stadium'] = i['ground']['longName']
+            datas['format'] = i['format']
+            datas['date'] = i['startDate']
             matches.append(datas)
 
     return matches
